310e73fb9f44924c8b11b674661edd04b11b208a
Priyanka yadav <priyanyadav52004@gmail.com>
2024-11-08 01:24:29 +0530

    add reagister page

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
-import LoginPage from "@/pages/Login";
+import LoginPage from "@/pages/LoginPage";
 import HomePage from "@/pages/HomePage";
+import RegisterPage from "@/pages/RegisterPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
   },
   {
     path: "/login",
