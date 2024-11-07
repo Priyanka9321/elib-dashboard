c1429dc60ac671bebab450c708cc57b53143a110
Priyanka yadav <priyanyadav52004@gmail.com>
2024-11-07 22:11:01 +0530

    add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@/components/ui/button"
 
-function App() {
+export default function App() {
   return (
-    <h1>Hello</h1>
+    <div>
+      <Button>Click me</Button>
+    </div>
   )
 }
-
-export default App
